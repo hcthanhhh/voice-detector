ffcc37e356822e1d03f5bb615950ce450da16fa8
hcthanhhh <44112010+hcthanhhh@users.noreply.github.com>
2019-05-20 17:04:59 +0700

    123

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Menus from '@/components/menus'
-import Products from '@/components/products'
-import Banner from '@/components/banner'
-import VoiceRecognition from '@/components/VoiceRecognition'
+import HelloWorld from '../components/HelloWorld'
+import Menus from '../components/menus'
+import Products from '../components/products'
+import Banner from '../components/banner'
+import VoiceRecognition from '../components/VoiceRecognition'
 Vue.use(Router)
 
 export default new Router({
