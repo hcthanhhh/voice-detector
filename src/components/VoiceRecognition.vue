43cbc68f8bc5f66a8402664674d328cbca33c1fe
hcthanhhh <44112010+hcthanhhh@users.noreply.github.com>
2019-05-21 15:56:32 +0700

    everything

--- src/components/VoiceRecognition.vue
+++ src/components/VoiceRecognition.vue
@@ -38,7 +38,7 @@ export default {
     init: function () {
       this.recognition.continuous = true
       this.recognition.interimResults = true
-      this.recognition.lang = vi-VN
+      this.recognition.lang = "vi-VN"
       this.reset()
       this.recognition.onend = this.reset()
       this.recognition.onresult = (event) => {
