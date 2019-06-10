77b6edafee827d04a048720fa6e1d77621164d62
Thanh Chan Hong <hcthanh@users.noreply.github.com>
2019-06-10 19:15:20 +0700

    already pull

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ var path = require('path');
 var serveStatic = require('serve-static');
 const app = express();
 app.use('/', serveStatic(path.join(__dirname + "/dist")));
-<<<<<<< HEAD
 // var port = process.env.PORT || 5000;
 var port = 80;
-=======
-var port = process.env.PORT || 3000;
->>>>>>> 12d60dd146fb2a8673e80b89d1d9c0952d913518
 app.listen(port);
 console.log('server started '+ port);
